4c1ab3ea92bb2cd7092495717380aa6034c7a1ad
akshay kumar <98104898+Flamer95711@users.noreply.github.com>
2024-07-08 16:34:09 +0530

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -8,7 +8,7 @@ class errHandler extends Error{
 export const errorHandling = (err, req, res, next) => {
   //console.log(err.message)
   err.message=err.message||"internal server error"
-  err.statusCode=err.statusCode||"500"
+  err.statusCode=err.statusCode||500
   res.status(err.statusCode).json({
     success: false,
     message: err.message,
